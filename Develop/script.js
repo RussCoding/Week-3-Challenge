2ff19c33976da96872d7aa7a4efb50fd4d90c19a
unknown <russelhkamal@gmail.com>
2022-03-20 23:25:53 -0400

    Mostly finished nested loop and if statements

--- Develop/script.js
+++ Develop/script.js
@@ -5,13 +5,14 @@ var numString = ["0","1","2","3","4","5","6","7","8","9"]
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 var specials = [" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/",":", 
   ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
-
-  var questionsArr = ["Would you like to include lowercase letters?", 
-                      "Would you like to include uppercase letters?",
-                      "Would you like to include numbers?",
-                      "Would you like to include special characters?",
-                      "Please enter a number from 8 to 128 for your password length."
+var questionsArr = ["Would you like to include lowercase letters?", 
+                    "Would you like to include uppercase letters?",
+                    "Would you like to include numbers?",
+                    "Would you like to include special characters?",
+                    "Please enter a number from 8 to 128 for your password length."
                   ];
+var charTypes = [lowercase, uppercase, numString, specials];
+var validChar = [];
 
 
 var input = document.createElement("input");
@@ -24,13 +25,43 @@ var generateBtn = document.querySelector("#generate");
 // Variable to access card header for questions
 var question = document.querySelector("#question");
 var answerArea = document.querySelector(".card-body");
+var passLength = 0;
 
 // Write password to the #password input
 function writePassword() {
   answerArea.appendChild(input);
+  // loop for answering each question
   for (let q = 0; q < questionsArr.length; q++)
   {
       question.value = questionsArr[q];
+      
+      var validAns = false;
+      // loop to stay on the same question until valid answer is input
+      while (validAns === false) {
+        //Checks for yes or know for all yes or no questions
+        if (q >= 0 && q < 4)
+        {
+          if (input.value.toUpperCase === "YES" || input.value.toUpperCase === "Y")
+          {
+            validAns = true;
+            validChar.concat(charTypes[q]);
+          }
+          else if (input.value.toUpperCase === "NO" || input.value.toUpperCase === "N")
+          {
+            validAns = true;
+          }
+        }
+        //case for last question where a number between 8-128 is needed
+        else
+        {
+          var ans = parseInt(input.value);
+          if (8 >= ans <= 128)
+            {
+              validAns = true;
+              passLength = ans;
+            }
+        }
+      }
   }
   
   var password = generatePassword();
