92fe9ba6f94eced67b6b0401d761da61217e0fea
unknown <russelhkamal@gmail.com>
2022-03-20 21:05:52 -0400

    added most of the required variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,15 @@
 // Assignment code here
-
-
+var lowercase = "abcdefghijklmnopqrstuvwxyz".split("");
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var specials = [" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/",":", 
+                ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+var input = document.createElement("input");
+input.type = "text";
+input.id = "answer"
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
+var question = document.querySelector("#question");
+var answerArea = document.querySelector("");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -13,5 +19,9 @@ function writePassword() {
 
 }
 
+
+function generatePassword(){
+
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
